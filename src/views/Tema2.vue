4096f3dddf7e4d67d599d8a92dc12c52dd5701fd
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-03-24 16:37:01 -0500

    Update repositorio

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -110,16 +110,15 @@
             figure
               img(data-aos="zoom-out-left" src='@/assets/curso/temas/texto_destacado/img21.svg', alt='imagen representativa')
 
-    .bloque-texto-a.color-primario.p-3.p-sm-4.p-md-4.mb-4(style="background-color : #E8E8E8")
-      .row.m-0
-        .col-lg-5
-          figure
-            img(data-aos="zoom-out-left" src='@/assets/curso/temas/texto_destacado/img22.png', alt='imagen representativa')
+    .row.justify-content-center.align-items-center          
+      .col-lg-10    
+        .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5.mb-5
+          .bloque-texto-g__img.p-5(
+            :style="{'background-image': `url(${require('@/assets/curso/temas/texto_destacado/img22.png')})`}"
+          )
+          .bloque-texto-g__texto.p-5
+            p.mb-0 Recuerde que todas las ideas y novedades que surgen a partir del proceso creativo o de innovación, tienen propósitos muy claros de solución o aporte en situaciones de necesidad o de proyección de mejoramiento de un producto, proceso o servicio. Ahora bien, las aplicaciones de estas técnicas, tanto del proceso creativo como innovativo, tienen varios campos de acción; en nuestro caso están asociadas al impacto organizacional, entendiendo que es una alternativa para mejorar y hacer más eficiente y eficaz la empresa.      
 
-        .col-lg-7
-          .bloque-texto-a__texto.p-4
-            p Recuerde que todas las ideas y novedades que surgen a partir del proceso creativo o de innovación, tienen propósitos muy claros de solución o aporte en situaciones de necesidad o de proyección de mejoramiento de un producto, proceso o servicio. Ahora bien, las aplicaciones de estas técnicas, tanto del proceso creativo como innovativo, tienen varios campos de acción; en nuestro caso están asociadas al impacto organizacional, entendiendo que es una alternativa para mejorar y hacer más eficiente y eficaz la empresa. 
-    
     p.mb-4 Para dar claridad sobre el tema se presentan los ámbitos de desarrollo para la aplicación de estos conceptos:
 
     .row.justify-content-around.mb-5
